3778782109854fd5697953d0f089176aec1e68c1
DenisTernovoy <96496534+DenisTernovoy@users.noreply.github.com>
2025-09-14 22:42:19 +0300

    post_test

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
+import json
 host_name = "localhost"
 server_port = 8080
+
 
 class MyServer(BaseHTTPRequestHandler):
     """Класс для обработки входящих запросов"""
@@ -18,22 +19,47 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes(data, 'utf-8'))
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        print(body)
         self.send_response(200)
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
+
+        content_length = int(self.headers['Content-Length'])
+
+        post_data = self.rfile.read(content_length)
+
+        data = json.loads(post_data.decode('utf-8'))
+
+        print('Имя:', data.get('name'))
+        print('Почта:', data.get('email'))
+        print('Сообщение:', data.get('message'))
+
+        response = {'status': 'success', 'message': 'Данные получены'}
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+
 
 if __name__ == "__main__":
     web_server = HTTPServer((host_name, server_port), MyServer)
 
     try:
-        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+
         web_server.serve_forever()
     except KeyboardInterrupt:
-        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+
         pass
 
-        # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+
     web_server.server_close()
     print("Server stopped.")
+
+    from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+
+
+
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Server running on port {port}...')
+    httpd.serve_forever()
+
